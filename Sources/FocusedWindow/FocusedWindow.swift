0fe00ae623e1219a1453ed08e8678e3ffff838b1
Sanav Pai <sanavpaipka@gmail.com>
2021-09-15 14:12:30 -0500

    Added a whole bunch of code dealing with window management and initialization of variables

--- Sources/FocusedWindow/FocusedWindow.swift
+++ Sources/FocusedWindow/FocusedWindow.swift
@@ -1,6 +1,33 @@
+import Foundation
+import AppKit
+import CoreGraphics
+
 public struct FocusedWindow {
-    
+//    Metadataa of
+    public var currentFocus : WindowMetadata
+//    activeApplication instance. Optional type because there may be a nil active app
     public init() {
+        let activeApp = NSWorkspace.shared.frontmostApplication!
+        let activeWindowBundleIdentifier = activeApp.bundleIdentifier!
+        let activeWindowId = activeApp.processIdentifier
         
+        let options = CGWindowListOption(arrayLiteral: CGWindowListOption.excludeDesktopElements, CGWindowListOption.optionOnScreenOnly)
+        let CGwindows = (CGWindowListCopyWindowInfo(options, CGWindowID(0))!) as NSArray
+        var activeWindowData = NSDictionary()
+        for i in Range(uncheckedBounds: (0, CGwindows.count)) {
+            let windowData = CGwindows[i] as! NSDictionary
+            let windowId = windowData.value(forKey: "kCGWindowOwnerPID") as! Int32
+            if (activeWindowId == windowId) {
+                activeWindowData = windowData
+                break;
+            }
+        }
+
+        currentFocus = WindowMetadata(windowId: activeWindowId, windowBundleIdentifier: activeWindowBundleIdentifier, windowData: activeWindowData)
+        
+    }
+    
+    public func getMetadata() -> WindowMetadata {
+        return currentFocus
     }
 }
---
+++ Sources/FocusedWindow/WindowMetadata.swift
@@ -0,0 +1,54 @@
+//
+//  File.swift
+//  
+//
+//  Created by Pranav Sai on 9/14/21.
+//
+
+import Foundation
+
+public struct WindowMetadata {
+    private let windowId : Int32
+    private let windowBundleIdentifier : String
+    private let windowData : NSDictionary
+    
+    public init(windowId: Int32, windowBundleIdentifier: String, windowData: NSDictionary) {
+        self.windowId = windowId
+        self.windowBundleIdentifier = windowBundleIdentifier
+        self.windowData = windowData
+    }
+    
+    public func getId() -> Int32 {
+        return windowId
+    }
+    
+    public func getBundleIdentifier() -> String {
+        return windowBundleIdentifier
+    }
+    
+    public func getData() -> NSDictionary {
+        return windowData
+    }
+    
+    public func getName() -> String {
+        return windowData.value(forKey: "kCGWindowOwnerName") as! String
+    }
+    
+    public func getFrame() -> NSRect {
+        let windowBoundsData = windowData.value(forKey: "kCGWindowBounds") as! NSDictionary
+        let windowX = windowBoundsData["X"]
+        let windowY = windowBoundsData["Y"]
+        let windowWidth = windowBoundsData["Width"]
+        let windowHeight = windowBoundsData["Height"]
+        var windowBounds = NSRect()
+        windowBounds.origin.x = windowX as! CGFloat;
+        windowBounds.origin.y = windowY as! CGFloat;
+        windowBounds.size.width = windowWidth as! CGFloat;
+        windowBounds.size.height = windowHeight as! CGFloat;
+        return windowBounds
+    }
+    
+    public func getLevel() -> Int {
+        return windowData.value(forKey: "kCGWindowLayer") as! Int
+    }
+}
